0364e87836853e0ec68a02e5f0fe45b979a9a2f1
Nicolas Moreira Zanatta <nicolas.mzanatta93@gmail.com>
2021-04-29 20:42:06 -0300

    primeira parte aula

---
+++ main.py
@@ -0,0 +1,30 @@
+if __name__ == '__main__':
+    temperatura = 20
+    print(type(temperatura))
+
+    temperatura = "Filipe"
+    print(type(temperatura))
+
+    listaNomes = ['Filipe', 'Daniel', 'Rafa', 'Douglas']
+    print(type(listaNomes))
+
+    idade = 29
+    print(type(idade))
+
+    dicionarioPessoa = {
+        'nome': 'Filipe',
+        'idade': idade
+    }
+
+    print(dicionarioPessoa)
+    print(type(dicionarioPessoa))
+
+    numeroComplexo = 5 + 5j
+    print(type(numeroComplexo))
+
+    verificacao = False
+    print(verificacao)
+
+    # Comentario no python
+
+
